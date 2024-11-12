041600ea7a8cb0d2bf9c54b086b04ee57d8a7f05
Sergio Saavedra <32399130+ssaavedraM@users.noreply.github.com>
2024-11-12 13:21:35 -0300

    first commit

---
+++ src/database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+
+    host='sql10.freemysqlhosting.net',
+    user='sql10744299',
+    password='NmbSWcV39h',
+    database='sql10744299'
+)
